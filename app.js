2d37c710d51b0d99d0ea1eaadba930e0338f87a3
Md Nazmul Islam <55467254+devnazmul@users.noreply.github.com>
2020-12-17 23:22:15 +0600

    Init commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let btn = document.getElementById("btn");
+let nav = document.getElementById("nav");
+let clocked = false;
+function menuToggle(){
+	nav.classList.toggle("extand");
+	btn.classList.toggle("extand2");
+}
