9f9386d8800ec8561169a22bc29081f97742a6b2
Md Nazmul Islam <55467254+devnazmul@users.noreply.github.com>
2020-12-18 09:29:56 +0600

    Add Cross Icon

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ let nav = document.getElementById("nav");
 let clocked = false;
 function menuToggle(){
 	nav.classList.toggle("extand");
+        btn.innerHTML = "<i class = 'fas fa-times'></i>";
 	btn.classList.toggle("extand2");
 }
