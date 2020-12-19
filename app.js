0b2bcbbef11bf29922f8e2b6db448b6b91db805c
Md Nazmul Islam <55467254+devnazmul@users.noreply.github.com>
2020-12-19 09:13:47 +0600

    Fixed code and add icon changing function

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 let btn = document.getElementById("btn");
 let nav = document.getElementById("nav");
-let clocked = false;
+
+//MENU TOGGLE
 function menuToggle(){
 	nav.classList.toggle("extand");
-        btn.innerHTML = "<i class = 'fas fa-times'></i>";
 	btn.classList.toggle("extand2");
+}
+//ICON TOGGLLE
+function iconToggle(){
+	if(btn.innerHTML == '<i class="fas fa-times"></i>'){
+		btn.innerHTML = '<i class="fas fa-bars"></i>';
+	}else{
+		btn.innerHTML = '<i class="fas fa-times"></i>';
+	}
 }
